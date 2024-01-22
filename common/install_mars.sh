a4abe73cbf966c1fff454db078be1ce31c2c90d3
majh <James.Hawkes@ecmwf.int>
2024-01-22 20:32:23 +0000

    refactor for multistage build

--- common/install_mars.sh
+++ common/install_mars.sh
@@ -31,5 +31,5 @@ echo "deb ${MARS_REPO}/private-debian-buster-stable/ buster main" >> /etc/apt/so
 apt-key add stable-public.gpg.key
 apt-get update
 apt install -y libnetcdf13 liblapack3
-apt install -y mars-client-cpp=0.0.3.0
+apt install -y mars-client-cpp=6.99.0.13
 apt install -y liblapack3 mars-client=6.32.1.0 mars-client-cloud=0.2.1
